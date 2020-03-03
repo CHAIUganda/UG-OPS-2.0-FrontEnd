43bc67b7be0880e04022341a498dfde631fcee68
Abel Oboth <aoboth@UG-PC0SYL23.clintonhealthaccess.org>
2020-03-03 16:31:49 +0300

    Add public holidays to home sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import DayPicker from 'react-day-picker';
+import axios from 'axios';
 
+import { BASE_URL } from '../../config';
 import HR from './hr';
 import './sidebar.css';
+import 'react-day-picker/lib/style.css';
 
 const mapStateToProps = (state) => ({
   token: state.auth.token,
@@ -11,11 +15,48 @@ const mapStateToProps = (state) => ({
 });
 
 function Sidebar({ token, section }) {
+  const [selectedDates, setSelectedDates] = useState([]);
+
+  const returnCalendar = () => {
+    if (!section) {
+      return (<div className="sidebarCalendar text-center">
+        <h4>Public Holidays</h4>
+        <DayPicker
+          selectedDays={selectedDates}
+          month={new Date()}
+          todayButton="Go to Today"
+          modifiers={{
+            foo: new Date(),
+          }}
+          // onTodayButtonClick={(day, modifiers) => console.log(day, modifiers)}
+        />
+      </div>);
+    }
+
+    return '';
+  };
+
+  useEffect(() => {
+    const endPoint = `${BASE_URL}hrApi/getPublicHolidays`;
+    axios.defaults.headers.common = { token };
+    axios.get(endPoint)
+      .then((res) => {
+        const days = [];
+        res.data.forEach((day) => {
+          days.push(new Date(`${new Date().getFullYear()}, ${day.date.split('/')[1]}, ${day.date.split('/')[0]}`));
+        });
+        setSelectedDates(days);
+      })
+      .catch((/* err */) => {
+      });
+  });
+
   return (
     <div className="setSideBarInline sidebgColor col-3 ml-3 position-fixed">
       <div className="row">
         <div className="col-12">
           <div className={`${token ? '' : 'sidebarContainer'}`}>
+            { returnCalendar() }
             <h3 className="text-center">{ section }</h3>
             {section === 'Human Resource'
               && <div className="navContainer">
--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 export const BASE_URL = 'http://45.79.187.217:3040/';
+
+
+// axios.defaults.headers.common = { token: tokenToSet };
