46a70b541f9989e5bf17f0c1abbeec5364f1590e
Abel Oboth <aoboth@UG-PC0SYL23.clintonhealthaccess.org>
2020-03-20 13:32:06 +0300

    Filter consolidated tracker by most parameters

--- src/components/content/hr/leaveManagement/consolidatedTracker/index.js
+++ src/components/content/hr/leaveManagement/consolidatedTracker/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import ReactToPrint from 'react-to-print';
+import html2canvas from 'html2canvas';
+import JSPDF from 'jspdf';
 
 import CommonSpinner from '../../../../common/spinner';
 import { BASE_URL, returnStatusClass } from '../../../../../config';
@@ -18,13 +19,27 @@ const mapStateToProps = (state) => ({
 function ConsolidatedTracker({ token }) {
   const [spinner, setSpinner] = useState(false);
   const [error, setError] = useState('');
-  const [/* allLeaves */, setallLeaves] = useState([]);
+  const [allLeaves, setallLeaves] = useState([]);
   const [filteredLeaves, setFilteredLeaves] = useState([]);
   const [typeFilter, setTypeFilter] = useState('all');
-  const [currentControl, setCurrentControl] = useState(null);
-  const [currentList, setCurrentList] = useState([]);
+  const [statusFilter, setStatusFilter] = useState('all');
+  const [startDate, setStartDate] = useState('all');
+  const [endDate, setEndDate] = useState('all');
+  const [allFiltersState, setAllFiltersState] = useState(
+    {
+      type: 'all',
+      status: 'all',
+      startDate: 'all',
+      endDate: 'all'
+    }
+  );
 
-  const componentRef = useRef();
+  let allFilters = {
+    type: 'all',
+    status: 'all',
+    startDate: 'all',
+    endDate: 'all'
+  };
 
   useEffect(() => {
     setSpinner(true);
@@ -66,51 +81,118 @@ function ConsolidatedTracker({ token }) {
     );
   }
 
-  const handleChange = (event, control) => {
-    let array2Filter = [];
-    if (control === 'typeFilter') {
-      const { value } = event.target;
-      if (currentControl !== control) {
-        setCurrentControl(control);
-        setCurrentList([...filteredLeaves]);
-        array2Filter = [...filteredLeaves];
+  const filter = (filterObj) => {
+    let array2Filter = [...allLeaves];
+    Object.keys(filterObj).forEach((fil) => {
+      if (filterObj[fil] === 'all') {
+        array2Filter = array2Filter.filter((leave) => leave[fil]);
+      } else if (fil === 'startDate' || fil === 'endDate') {
+        array2Filter = array2Filter.filter((leave) => `${new Date(leave[fil]).getMonth()}` === allFilters[fil]);
       } else {
-        array2Filter = [...currentList];
+        array2Filter = array2Filter.filter((leave) => leave[fil] === allFilters[fil]);
       }
-
-      setTypeFilter(value);
-      if (value && value !== 'all') {
-        const l = array2Filter.filter((leave) => leave.type === value);
-        setFilteredLeaves(l);
-      }
-    }
+    });
+    setFilteredLeaves(array2Filter);
   };
 
+  const handleChange = (event, stateSetter, filterParam) => {
+    event.preventDefault();
+    const { value } = event.target;
+    stateSetter(value);
+    allFilters = {
+      ...allFiltersState,
+      [filterParam]: value
+    };
+    setAllFiltersState(allFilters);
+    filter(allFilters);
+  };
+
+  const returnTypeFilterHead = () => (
+    <th scope="col">
+            Type
+      <select className="dropdownFilter" value={typeFilter} onChange={(e) => handleChange(e, setTypeFilter, 'type')}>
+        <option value="all" className="optionTableStyle">all</option>
+        <option value="Annual" className="optionTableStyle">Annual</option>
+        <option value="Paternity" className="optionTableStyle">Paternity</option>
+        <option value="Home" className="optionTableStyle">Home</option>
+        <option value="Maternity" className="optionTableStyle">Maternity</option>
+        <option value="Sick" className="optionTableStyle">Sick</option>
+        <option value="Unpaid" className="optionTableStyle">Unpaid</option>
+        <option value="Study" className="optionTableStyle">Study</option>
+      </select>
+    </th>
+  );
+
+  const returnStatusFilterHead = () => (
+    <th scope="col">
+            Status
+      <select className="dropdownFilter" value={statusFilter} onChange={(e) => handleChange(e, setStatusFilter, 'status')}>
+        <option value="all" className="optionTableStyle">all</option>
+        <option value="Planned" className="optionTableStyle">Planned</option>
+        <option value="Pending Supervisor" className="optionTableStyle">Pending Supervisor</option>
+        <option value="Cancelled" className="optionTableStyle">Cancelled</option>
+        <option value="Supervisor Declined" className="optionTableStyle">Supervisor Declined</option>
+        <option value="Country Director Declined" className="optionTableStyle">Country Director Declined</option>
+        <option value="Approved" className="optionTableStyle">Approved</option>
+        <option value="Taken" className="optionTableStyle">Taken </option>
+        <option value="Not taken" className="optionTableStyle">Not taken </option>
+      </select>
+    </th>
+  );
+
+  const returnStartMonthFilterHead = () => (
+    <th scope="col">
+            Starts
+      <select className="dropdownFilter" value={startDate} onChange={(e) => handleChange(e, setStartDate, 'startDate')}>
+        <option value="all" className="optionTableStyle">all</option>
+        <option value="0" className="optionTableStyle">Jan</option>
+        <option value="1" className="optionTableStyle">Feb</option>
+        <option value="2" className="optionTableStyle">Mar</option>
+        <option value="3" className="optionTableStyle">Apr</option>
+        <option value="4" className="optionTableStyle">May</option>
+        <option value="5" className="optionTableStyle">Jun</option>
+        <option value="6" className="optionTableStyle">Jul </option>
+        <option value="7" className="optionTableStyle">Aug </option>
+        <option value="8" className="optionTableStyle">Sep </option>
+        <option value="9" className="optionTableStyle">Oct </option>
+        <option value="10" className="optionTableStyle">Nov </option>
+        <option value="11" className="optionTableStyle">Dec</option>
+      </select>
+    </th>
+  );
+
+  const returnEndMonthFilterHead = () => (
+    <th scope="col">
+            Ends
+      <select className="dropdownFilter" value={endDate} onChange={(e) => handleChange(e, setEndDate, 'endDate')}>
+        <option value="all" className="optionTableStyle">all</option>
+        <option value="0" className="optionTableStyle">Jan</option>
+        <option value="1" className="optionTableStyle">Feb</option>
+        <option value="2" className="optionTableStyle">Mar</option>
+        <option value="3" className="optionTableStyle">Apr</option>
+        <option value="4" className="optionTableStyle">May</option>
+        <option value="5" className="optionTableStyle">Jun</option>
+        <option value="6" className="optionTableStyle">Jul </option>
+        <option value="7" className="optionTableStyle">Aug </option>
+        <option value="8" className="optionTableStyle">Sep </option>
+        <option value="9" className="optionTableStyle">Oct </option>
+        <option value="10" className="optionTableStyle">Nov </option>
+        <option value="11" className="optionTableStyle">Dec</option>
+      </select>
+    </th>
+  );
+
   const returnData = () => (
-    <table className="table holidaysTable" id="hrConsolidatedTrackerTable" ref={componentRef}>
+    <table className="table holidaysTable" id="hrConsolidatedTrackerTable">
       <thead>
         <tr>
           <th scope="col">Name</th>
           <th scope="col">Program</th>
-          <th scope="col">
-            Type
-            <select className="dropdownFilter" value={typeFilter} onChange={(e) => handleChange(e, 'typeFilter')}>
-              <option value="all" className="optionTableStyle">all</option>
-              <option value="Annual" className="optionTableStyle">Annual</option>
-              <option value="Paternity" className="optionTableStyle">Paternity</option>
-              <option value="Home" className="optionTableStyle">Home</option>
-              <option value="Maternity" className="optionTableStyle">Maternity</option>
-              <option value="Sick" className="optionTableStyle">Sick</option>
-              <option value="Unpaid" className="optionTableStyle">Unpaid</option>
-              <option value="Study" className="optionTableStyle">Study</option>
-            </select>
-          </th>
-          <th scope="col">Status</th>
-          <th scope="col">Rejection Reason</th>
+          {returnTypeFilterHead()}
+          {returnStatusFilterHead()}
           <th scope="col">Days</th>
-          <th scope="col">Starts</th>
-          <th scope="col">Ends</th>
-          <th scope="col">Month</th>
+          {returnStartMonthFilterHead()}
+          {returnEndMonthFilterHead()}
         </tr>
       </thead>
       <tbody>
@@ -125,7 +207,6 @@ function ConsolidatedTracker({ token }) {
                   {leave.status}
                 </button>
               </td>
-              <td>{leave.rejectionReason ? leave.rejectionReason : '-'}</td>
               <td>{leave.daysTaken}</td>
               <td>{new Date(leave.startDate).toDateString()}</td>
               <td>{new Date(leave.endDate).toDateString()}</td>
@@ -136,17 +217,31 @@ function ConsolidatedTracker({ token }) {
     </table>
   );
 
+  const generatePDf = (event) => {
+    event.preventDefault();
+    const input = document.getElementById('hrConsolidatedTrackerTable');
+
+    html2canvas(input)
+      .then((canvas) => {
+        // const imgData = canvas.toDataURL('image/png');
+        const imgData = canvas.toDataURL('image/jpeg');
+
+        const pdf = new JSPDF({
+          orientation: 'landscape'
+        });
+        pdf.addImage(imgData, 'jpeg', 0, 0, 0, 0);
+        pdf.save('download.pdf');
+      });
+  };
+
   return (
     <div className="row">
       <div className="col">
         <h3>
-            Consolidated Leave Tracker
-          <ReactToPrint
-            trigger={() => <button type="button" className="btn btn-secondary float-right">
-            Print Current Selection
-            </button>}
-            content={() => componentRef.current}
-          />
+            Consolidated Leave ConsolidatedTracker
+          <button type="button" className="btn btn-secondary float-right" onClick={generatePDf}>
+            Generate PDF
+          </button>
         </h3>
         <div className="row">
           <div className="col">
