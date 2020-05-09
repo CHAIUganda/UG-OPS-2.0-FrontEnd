51cbff9494470bb2408ba3241ae61565261d4b8c
Abel Oboth <aoboth@UG-PC0SYL23.clintonhealthaccess.org>
2020-05-09 10:43:21 +0300

    Change view of modal based on status

--- src/components/content/hr/leaveManagement/applyForLeave/manageLeaveModal/index.js
+++ src/components/content/hr/leaveManagement/applyForLeave/manageLeaveModal/index.js
@@ -184,131 +184,210 @@ export default function ManageLeaveModal({
     );
   };
 
-  return (
-    <div>
-      <span
-        onClick={toggle}
-        className="pointerCursor">
-        <IconContext.Provider value={{ size: '2em' }}>
-          <IoMdSettings />
-        </IconContext.Provider>
-      </span>
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Manage Leave</ModalHeader>
-        <ModalBody>
-          {
-            error
-            && <div className="errorFeedback row">
+  /*
+    convert to lower case and compare with paul's list then return.
+  */
+
+  const returnLeaveDetails = () => {
+    if (
+      leaveStatus.toLowerCase() === 'planned'
+      || leaveStatus.toLowerCase() === 'approved'
+      || leaveStatus.toLowerCase() === 'taken'
+      || leaveStatus.toLowerCase() === 'pending supervisor'
+      || leaveStatus.toLowerCase() === 'pending country director'
+    ) {
+      return (
+        <div>
+          <span
+            onClick={toggle}
+            className="pointerCursor">
+            <IconContext.Provider value={{ size: '2em' }}>
+              <IoMdSettings />
+            </IconContext.Provider>
+          </span>
+          <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Manage Leave</ModalHeader>
+            <ModalBody>
+              {
+                error
+                  && <div className="errorFeedback row">
+                    <div className="col">
+                      {error}
+                    </div>
+                  </div>
+              }
+              {
+                successFeedback
+                  && <div className="successFeedback row">
+                    <div className="col">
+                      {successFeedback}
+                    </div>
+                  </div>
+              }
+              <div className="row">
+                <div className="col">
+                  <p>Status: <button className={returnStatusClass(leaveStatus)}>
+                    {leaveStatus}
+                  </button> </p>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col">
+                  <FormGroup>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>Supervisor</InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        type="text"
+                        value={supervisorName}
+                        disabled
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                  {/* Category */}
+                  <FormGroup>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>Category</InputGroupText>
+                      </InputGroupAddon>
+                      <CustomInput
+                        type="select"
+                        id="exampleCustomSelect"
+                        name="customSelect"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                      >
+                        <option value="Annual">Annual Leave</option>
+                        {gender === 'Female'
+                            && <option value="Maternatiy">Maternity Leave</option>
+                        }
+                        {(gender === 'Male' || gender === 'male')
+                              && <option value="Paternity">Paternity Leave</option>
+                        }
+                        {
+                          (type === 'tcn' || type === 'expat')
+                      && <option value="Home">Home Leave</option>
+                        }
+                        <option value="Study">Study Leave</option>
+                        <option value="Unpaid">Unpaid Leave</option>
+                      </CustomInput>
+                    </InputGroup>
+                  </FormGroup>
+                  {/* Comment */}
+                  <FormGroup>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>Comment</InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="Optional"
+                        type="text"
+                        value={comment}
+                        onChange={(e) => {
+                          setSuccessFeedback('');
+                          setError('');
+                          setComment(e.target.value);
+                        }}
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                  {/*  leave Date */}
+                  <FormGroup>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>Select Range of Leave</InputGroupText>
+                      </InputGroupAddon>
+                      <Calendar
+                        value={leaveDates}
+                        selectRange={true}
+                        onChange={(date) => {
+                          setSuccessFeedback('');
+                          setError('');
+                          setError('');
+                          setLeaveDate(date);
+                        }
+                        } />
+                    </InputGroup>
+                  </FormGroup>
+                </div>
+              </div>
+
+              <ModalFooter>
+                {returnModalFooter()}
+              </ModalFooter>
+            </ModalBody>
+          </Modal>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <span
+          onClick={toggle}
+          className="pointerCursor">
+          <IconContext.Provider value={{ size: '2em' }}>
+            <IoMdSettings />
+          </IconContext.Provider>
+        </span>
+        <Modal isOpen={modal} toggle={toggle}>
+          <ModalHeader toggle={toggle}>Manage Leave</ModalHeader>
+          <ModalBody>
+            {
+              error
+                  && <div className="errorFeedback row">
+                    <div className="col">
+                      {error}
+                    </div>
+                  </div>
+            }
+            {
+              successFeedback
+                  && <div className="successFeedback row">
+                    <div className="col">
+                      {successFeedback}
+                    </div>
+                  </div>
+            }
+            {/* <div className="row">
               <div className="col">
-                {error}
+                <p>Status: </span><button className={returnStatusClass(leaveStatus)}>
+                  {leaveStatus}
+                </button> </p>
+              </div>
+            </div> */}
+
+            <div className="row">
+              <div className="col">
+                <p> <span className="blur">To Start:</span> {new Date(leave.startDate).toDateString()}</p>
+                <p><span className="blur">Category:</span> {leave.type} </p>
+                <p><span className="blur">Comment: </span>{leave.comment} </p>
+              </div>
+              <div className="col">
+                <p><span className="blur">To End: </span> {new Date(leave.endDate).toDateString()} </p>
+                <p><span className="blur">Status: </span> <button className={returnStatusClass(leaveStatus)}>
+                  {leaveStatus}
+                </button> </p>
+                { leave.rejectionReason
+                  && <p> <span className="blur">Reason for declining your leave:</span>
+                    {`   ${leave.rejectionReason}`}
+                  </p>
+                }
+              </div>
+
+              <div className="col">
+                <p><span className="blur">Days To Take:</span> {leave.daysTaken} </p>
               </div>
             </div>
-          }
-          {
-            successFeedback
-            && <div className="successFeedback row">
-              <div className="col">
-                {successFeedback}
-              </div>
-            </div>
-          }
-          <div className="row">
-            <div className="col">
-              <p>Status: <button className={returnStatusClass(leaveStatus)}>
-                {leaveStatus}
-              </button> </p>
-            </div>
-          </div>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  };
 
-          <div className="row">
-            <div className="col">
-              <FormGroup>
-                <InputGroup>
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>Supervisor</InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    type="text"
-                    value={supervisorName}
-                    disabled
-                  />
-                </InputGroup>
-              </FormGroup>
-              {/* Category */}
-              <FormGroup>
-                <InputGroup>
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>Category</InputGroupText>
-                  </InputGroupAddon>
-                  <CustomInput
-                    type="select"
-                    id="exampleCustomSelect"
-                    name="customSelect"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                  >
-                    <option value="Annual">Annual Leave</option>
-                    {gender === 'Female'
-                      && <option value="Maternatiy">Maternity Leave</option>
-                    }
-                    {(gender === 'Male' || gender === 'male')
-                        && <option value="Paternity">Paternity Leave</option>
-                    }
-                    {
-                      (type === 'tcn' || type === 'expat')
-                && <option value="Home">Home Leave</option>
-                    }
-                    <option value="Study">Study Leave</option>
-                    <option value="Unpaid">Unpaid Leave</option>
-                  </CustomInput>
-                </InputGroup>
-              </FormGroup>
-              {/* Comment */}
-              <FormGroup>
-                <InputGroup>
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>Comment</InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="Optional"
-                    type="text"
-                    value={comment}
-                    onChange={(e) => {
-                      setSuccessFeedback('');
-                      setError('');
-                      setComment(e.target.value);
-                    }}
-                  />
-                </InputGroup>
-              </FormGroup>
-              {/*  leave Date */}
-              <FormGroup>
-                <InputGroup>
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>Select Range of Leave</InputGroupText>
-                  </InputGroupAddon>
-                  <Calendar
-                    value={leaveDates}
-                    selectRange={true}
-                    onChange={(date) => {
-                      setSuccessFeedback('');
-                      setError('');
-                      setError('');
-                      setLeaveDate(date);
-                    }
-                    } />
-                </InputGroup>
-              </FormGroup>
-            </div>
-          </div>
-
-          <ModalFooter>
-            {returnModalFooter()}
-          </ModalFooter>
-        </ModalBody>
-      </Modal>
-    </div>
-  );
+  return (returnLeaveDetails());
 }
 
 
