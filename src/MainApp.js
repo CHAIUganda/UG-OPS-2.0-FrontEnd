3beb5e8199c6b72ce7bd9ccf5d30eaa8e3889b6f
Abel Oboth <aoboth@UG-PC0SYL23.clintonhealthaccess.org>
2020-05-14 15:47:15 +0300

    Fix bugs before demo

--- src/MainApp.js
+++ src/MainApp.js
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Pusher from 'pusher-js';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import UIfx from 'uifx';
+import notificationSound from './when.mp3';
 
 import App from './App';
 import * as notificationActions from './redux/actions/notificationsActions';
@@ -15,27 +19,38 @@ const matchDispatchToProps = {
 };
 
 function MainApp({ AddNotification, email }) {
-  useEffect(() => {
-    const pusher = new Pusher('afacc8c93d042f2ec024', {
-      cluster: 'ap2',
-      encrypted: true
-    });
-    const channel = pusher.subscribe('notifications');
-    channel.bind('ugops2', (data) => {
-      if (email.trim() === data.staffEmail.trim()) {
-        AddNotification(data);
-      }
-    });
+  // debugger;
+  const beep = new UIfx({ asset: notificationSound });
+  if (email) {
+    useEffect(() => {
+      const handleNotifications = (data) => {
+        if (email === data.staffEmail) {
+          toast(data.title);
+          beep.play();
+          AddNotification(data);
+        }
+      };
 
-    return function cleanUp() {
-      if (channel) {
-        channel.unbind('ugops2');
-      }
-    };
-  }, []);
+      const pusher = new Pusher('afacc8c93d042f2ec024', {
+        cluster: 'ap2',
+        encrypted: true
+      });
+      const channel = pusher.subscribe('notifications');
+      channel.bind('ugops2', (data) => handleNotifications(data));
+
+      return function cleanUp() {
+        if (channel) {
+          channel.unbind('ugops2');
+        }
+      };
+    }, []);
+  }
 
   return (
-    <App />
+    <>
+      <ToastContainer />
+      <App />
+    </>
   );
 }
 
