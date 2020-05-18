df9308dcfdab14e3085828f756215bd2fd641333
Abel Oboth <aoboth@UG-PC0SYL23.clintonhealthaccess.org>
2020-05-18 08:47:29 +0300

    Add functional code to mark as read notification.

--- src/components/common/notifications/index.js
+++ src/components/common/notifications/index.js
@@ -11,21 +11,52 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { AiOutlineNotification } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+import * as notificationActions from '../../../redux/actions/notificationsActions';
+import { BASE_URL } from '../../../config';
 import Icon from '../icon';
 import './notifications.css';
 
+const matchDispatchToProps = {
+  removeNotification: notificationActions.removeNotification
+};
+
 const mapStateToProps = (state) => ({
   notifications: state.notifications,
+  token: state.auth.token,
+  email: state.auth.email
 });
 
-function Notifications({ notifications }) {
+function Notifications({
+  notifications,
+  token,
+  email,
+  removeNotification
+}) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const toggle = () => setDropdownOpen((prevState) => !prevState);
 
   const handleMarkAsRead = (notificationId) => {
-    console.log(notificationId);
+    axios.defaults.headers.common = { token };
+    const endPoint = `${BASE_URL}auth/handleNotifications`;
+    const notificationToDismiss = {
+      staffEmail: email,
+      notificationId
+    };
+
+    axios.post(endPoint, notificationToDismiss)
+      .then(() => {
+        removeNotification(notificationId);
+      })
+      .catch(() => {
+        // if (err && err.response && err.response.data && err.response.data.message) {
+        //   setError(err.response.data.message);
+        // } else {
+        //   setError(err.message);
+        // }
+      });
   };
 
   const returnNotificationItems = () => {
@@ -86,6 +117,9 @@ function Notifications({ notifications }) {
 
 Notifications.propTypes = {
   notifications: PropTypes.array,
+  token: PropTypes.string,
+  email: PropTypes.string,
+  removeNotification: PropTypes.func
 };
 
-export default connect(mapStateToProps)(Notifications);
+export default connect(mapStateToProps, matchDispatchToProps)(Notifications);
