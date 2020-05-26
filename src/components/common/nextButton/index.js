257e4dc8a0cc14e6288ee311633c29ca038b2c9e
Abel Oboth <aoboth@UG-PC0SYL23.clintonhealthaccess.org>
2020-05-26 10:14:35 +0300

    Reset the signin page.

--- src/components/common/nextButton/index.js
+++ src/components/common/nextButton/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
+import PropTypes from 'prop-types';
 
 import Icon from '../icon';
 
 import './nextButton.css';
 
-const NextButton = () => (
-  <span className='pointerCursor float-right nextButton'>
+const NextButton = ({
+  setCurrentComponent,
+  activeSections,
+  currentComponent
+}) => {
+  const handleClick = (event) => {
+    event.preventDefault();
+    const currentIndex = activeSections.indexOf(currentComponent[0]);
+    if (currentIndex !== -1 && currentIndex < activeSections.length) {
+      setCurrentComponent([activeSections[currentIndex + 1]]);
+    }
+  };
+
+  return (
+    <span className='pointerCursor float-right nextButton' onClick={handleClick}>
       Next
-    <Icon
-      Icon2Render={IoIosArrowForward}
-      color={'#003366'}
-    />
-  </span>
-);
+      <Icon
+        Icon2Render={IoIosArrowForward}
+        color={'#003366'}
+      />
+    </span>
+  );
+};
+
+NextButton.propTypes = {
+  setCurrentComponent: PropTypes.func,
+  setActiveSections: PropTypes.func,
+  currentComponent: PropTypes.object,
+  activeSections: PropTypes.array
+};
 
 export default NextButton;
