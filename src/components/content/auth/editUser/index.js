bfb1fe83f2532a2da9435f160a960dc8acb396c3
Abel Oboth <aoboth@UG-PC0SYL23.clintonhealthaccess.org>
2020-07-10 10:38:18 +0300

    Fix refresh bug on edit user.

--- src/components/content/auth/editUser/index.js
+++ src/components/content/auth/editUser/index.js
@@ -18,6 +18,9 @@ import Select from 'react-select';
 import { useOktaAuth } from '@okta/okta-react';
 
 import * as sideBarActions from '../../../../redux/actions/sideBarActions';
+import * as authActions from '../../../../redux/actions/authActions';
+import * as notificationActions from '../../../../redux/actions/notificationsActions';
+
 import CommonSpinner from '../../../common/spinner';
 import EditBankDetailsModal from './editBankDetails';
 import { BASE_URL, returnStatusClass } from '../../../../config';
@@ -25,7 +28,9 @@ import './editUser.css';
 
 const matchDispatchToProps = {
   changeSection: sideBarActions.changeSection,
-  changeActive: sideBarActions.changeActive
+  changeActive: sideBarActions.changeActive,
+  logUserIn: authActions.logUserIn,
+  setInitialNotifications: notificationActions.setInitialNotifications
 };
 
 const mapStateToProps = (state) => ({
@@ -40,7 +45,9 @@ function EditUser(props) {
     token,
     changeSection,
     changeActive,
-    roles
+    roles,
+    setInitialNotifications,
+    logUserIn
   } = props;
 
   if (token && roles) {
@@ -201,7 +208,7 @@ function EditUser(props) {
   const [accountNumber, setAccountNumber] = useState('');
   const [Currency, setCurrency] = useState('UGX');
 
-  const { authService } = useOktaAuth();
+  const { authState, authService } = useOktaAuth();
 
   const returnDefaultSupervisor = () => {
     if (user.supervisorDetails
@@ -325,8 +332,54 @@ function EditUser(props) {
       });
   };
 
-  useEffect(() => {
-    setSpinner(true);
+  const setUpUser = (tokenToSet) => {
+    axios.defaults.headers.common = { token: tokenToSet };
+    const apiRoute = `${BASE_URL}auth/getLoggedInUser`;
+    axios.get(apiRoute)
+      . then((res) => {
+        const {
+          department,
+          fName,
+          internationalStaff,
+          lName,
+          _id,
+          supervisorDetails,
+          notifications
+        } = res.data;
+        const genderToSet = res.data.gender;
+        const emailToSet = res.data.email;
+        const leaveDetailsToSet = res.data.leaveDetails;
+        const positionToSet = res.data.position;
+
+        const userObject = {
+          ...res.data,
+          email: emailToSet,
+          token: tokenToSet,
+          gender: genderToSet,
+          internationalStaff,
+          department,
+          firstName: fName,
+          lastName: lName,
+          Position: positionToSet,
+          id: _id,
+          leaveDetails: leaveDetailsToSet,
+          supervisor: supervisorDetails
+        };
+        setInitialNotifications(notifications);
+        logUserIn(userObject);
+        setSpinner(false);
+      })
+      .catch((err) => {
+        setSpinner(false);
+        if (err && err.response && err.response.data && err.response.data.message) {
+          setError(err.response.data.message);
+        } else {
+          setError(err.message);
+        }
+      });
+  };
+
+  const setUpThisPage = () => {
     axios.defaults.headers.common = { token };
     const apiRoute = `${BASE_URL}hrApi/getPrograms`;
     axios.get(apiRoute)
@@ -348,6 +401,25 @@ function EditUser(props) {
           setError(err.message);
         }
       });
+  };
+
+  useEffect(() => {
+    setSpinner(true);
+    setError('');
+
+    if (token) {
+      setUpThisPage();
+    }
+
+    if (!token && authState.isAuthenticated) {
+      const { accessToken } = authState;
+      setUpUser(`Bearer ${accessToken}`);
+    }
+
+    if (!token && !authState.isAuthenticated) {
+      setSpinner(false);
+      authService.logout('/');
+    }
   }, []);
 
   const onSelectSupervisorEmail = (value) => {
@@ -1045,7 +1117,9 @@ EditUser.propTypes = {
   propsPassed: PropTypes.bool,
   user: PropTypes.object,
   changeSection: PropTypes.func,
-  changeActive: PropTypes.func
+  changeActive: PropTypes.func,
+  setInitialNotifications: PropTypes.func,
+  logUserIn: PropTypes.func,
 };
 
 export default connect(mapStateToProps, matchDispatchToProps)(EditUser);
